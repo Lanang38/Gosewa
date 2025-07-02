a8d1fa8a025b90f23d21edc3178c71d9a7e699a7
naufal <naufalahmad281@gmail.com>
2025-07-03 01:40:09 +0700

    menambahkan id untuk google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-8QFM59N90Q");
+ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
