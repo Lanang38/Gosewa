de5c01f91fce0951db0f763e944814aadb07db74
Lanang38 <cokde38@gmail.com>
2025-07-07 18:25:57 +0700

    Refactor TopBar component for improved code formatting and readability

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,14 +1,16 @@
-import { section } from "framer-motion/client";
-import { Phone } from "lucide-react";
+import { section } from 'framer-motion/client';
+import { Phone } from 'lucide-react';
 
 export default function TopBar() {
   return (
-    <section id="contacts " className="overflow-hidden">
+    <section id="contacts" className="overflow-hidden">
       <div className="bg-blue-800 py-5 text-white">
         <div className="max-w-6xl mx-auto px-4 flex justify-between items-center text-sm">
           <p className="flex items-center gap-2">
             Punya Pertanyaan? Bisa Tanyakan Kami
-            <a target="blank" href="https://wa.me/081325716185"><Phone className="w-4 h-4" /></a>
+            <a target="blank" href="https://wa.me/081325716185">
+              <Phone className="w-4 h-4" />
+            </a>
           </p>
         </div>
       </div>
